49a51958ba009a55626b11980370988ea5d4d00a
ATTaylor23 <taylorthorntonalexander23@gmail.com>
2022-06-28 17:14:21 -0400

    2.0 commit

--- src/main/java/com/build/qa/build/selenium/pageobjects/BasePage.java
+++ src/main/java/com/build/qa/build/selenium/pageobjects/BasePage.java
@@ -1,6 +1,8 @@
 package com.build.qa.build.selenium.pageobjects;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.Wait;
 
@@ -13,4 +15,62 @@ public abstract class BasePage extends BaseFramework {
 		this.wait = wait;
 		PageFactory.initElements(driver, this);
 	}
+
+	//Pop Up Prompt
+
+	@FindBy(xpath = "//div[@id='newsletter-modal']")
+	public WebElement newsLetterPop;
+	@FindBy(xpath = "//button[@tabindex='-1']")
+	public WebElement newLetterX;
+
+	@FindBy(xpath = "//button[contains(text(),'Not Yet')]")
+	public WebElement notYet;
+	@FindBy(xpath = "//button[contains(text(),'Yes Please')]")
+	public WebElement yesPlease;
+
+	//Search
+	@FindBy(id = "search_txt")
+	public WebElement searchBox;
+	@FindBy(xpath = "//button[@class='button-primary search-site-search']")
+	public WebElement magnify;
+
+	@FindBy(xpath = "//div[@data-automation='no-search-results-page']//h1")
+	public WebElement searchResult;
+
+	//Bathroom
+	@FindBy(xpath = "//a[@data-category-id='108412']")
+	public WebElement bathroom;
+	@FindBy(xpath = "//a[@data-tracking='nav:menu:category:Bathroom:Bathroom Sinks']")
+	public WebElement sinks;
+
+	//Purchase
+	@FindBy(xpath = "//button[contains(text(),'Shop Now')]")
+	public WebElement shopNow;
+
+	@FindBy(xpath = "//img[@alt='Miseno']")
+	public WebElement sinkType;
+
+	@FindBy(xpath = "//button[@data-automation='add-to-cart-button']")
+	public WebElement addToCart;
+	@FindBy(xpath = "//div[@class='flex flex-column flex-row-ns items-center justify-center h-100']//div//div")
+	public WebElement shopCart;
+	@FindBy(xpath = "//div[@class='absolute right-0 top-0']")
+	public WebElement close;
+	@FindBy(xpath = "//button[@aria-label='Email to a friend']")
+	public WebElement emailFriend;
+
+	@FindBy(xpath = "//button[@aria-label='Email to a friend']")
+	public WebElement recipientName;
+	@FindBy(xpath = "//input[@name='recipientEmail']")
+	public WebElement recipientEmail;
+	@FindBy(xpath = "//input[@name='name']")
+	public WebElement yourName;
+	@FindBy(xpath = "//input[@name='email']")
+	public WebElement yourEmail;
+	@FindBy(xpath = "//input[@name='shouldSendCopy']")
+	public WebElement myCopy;
+	@FindBy(xpath = "//button[contains(text(),'Send to a Friend')]")
+	public WebElement send;
+
+
 }
--- src/test/java/com/build/qa/build/selenium/tests/BuildTest.java
+++ src/test/java/com/build/qa/build/selenium/tests/BuildTest.java
@@ -1,9 +1,17 @@
 package com.build.qa.build.selenium.tests;
 
+import org.apache.commons.io.FileUtils;
 import org.junit.Test;
 
 import com.build.qa.build.selenium.framework.BaseFramework;
 import com.build.qa.build.selenium.pageobjects.homepage.HomePage;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.interactions.Actions;
+
+import java.io.File;
+import java.io.IOException;
 
 public class BuildTest extends BaseFramework {
 
@@ -28,7 +36,24 @@ public class BuildTest extends BaseFramework {
 	 */
 	@Test
 	public void searchForProductLandsOnCorrectProduct() {
-		// TODO: Implement this test
+
+		driver.get(getConfiguration("HOMEPAGE"));
+		HomePage homePage = new HomePage(driver, wait);
+		JavascriptExecutor js = (JavascriptExecutor)driver;
+
+		if(homePage.newsLetterPop.isDisplayed()){
+			js.executeScript("arguments[0].click();", homePage.newLetterX);
+		}else if(homePage.yesPlease.isDisplayed()){
+			js.executeScript("arguments[0].click();", homePage.notYet);
+		}
+
+		homePage.searchBox.sendKeys("Quoizel MY1613");
+		js.executeScript("arguments[0].click();", homePage.magnify);
+
+		softly.assertThat(homePage.onBuildTheme())
+				.as("The page has \"Quoizel MY1613\" present")
+				.isTrue();
+
 	}
 
 	/**
@@ -39,7 +64,25 @@ public class BuildTest extends BaseFramework {
 	 */
 	@Test
 	public void addProductToCartFromCategoryDrop() {
-		// TODO: Implement this test
+
+		driver.get(getConfiguration("HOMEPAGE"));
+		HomePage homePage = new HomePage(driver, wait);
+		JavascriptExecutor js = (JavascriptExecutor)driver;
+
+		if(homePage.newsLetterPop.isDisplayed()){
+			js.executeScript("arguments[0].click();", homePage.newLetterX);
+		}else if(homePage.yesPlease.isDisplayed()){
+			js.executeScript("arguments[0].click();", homePage.notYet);
+		}
+
+		((JavascriptExecutor)driver).executeScript("$('element_selector').hover();", homePage.bathroom);
+		js.executeScript("arguments[0].click();", homePage.bathroom);
+
+
+		softly.assertThat(homePage.onBuildTheme())
+				.as("The URL contains bathroom-sinks")
+				.isTrue();
+
 	}
 
 	/**
@@ -49,8 +92,55 @@ public class BuildTest extends BaseFramework {
 	 * @difficulty Medium-Hard
 	 */
 	@Test
-	public void addProductToCartAndEmailIt() {
-		// TODO: Implement this test
+	public void addProductToCartAndEmailIt() throws IOException, InterruptedException {
+
+		driver.get(getConfiguration("HOMEPAGE"));
+		HomePage homePage = new HomePage(driver, wait);
+		JavascriptExecutor js = (JavascriptExecutor)driver;
+		Actions act = new Actions(driver);
+
+		if(homePage.newsLetterPop.isDisplayed()){
+			js.executeScript("arguments[0].click();", homePage.newLetterX);
+		}else if(homePage.yesPlease.isDisplayed()){
+			js.executeScript("arguments[0].click();", homePage.notYet);
+		}
+
+		((JavascriptExecutor)driver).executeScript("$('element_selector').hover();", homePage.bathroom);
+		js.executeScript("arguments[0].click();", homePage.bathroom);
+
+		js.executeScript("arguments[0].click();", homePage.shopNow);
+
+		((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", homePage.sinkType);
+		Thread.sleep(500);
+		//js.executeScript("window.scrollBy(0,2000)", "");
+
+
+		js.executeScript("arguments[0].click();", homePage.sinkType);
+		js.executeScript("arguments[0].click();", homePage.addToCart);
+		js.executeScript("arguments[0].click();", homePage.close);
+		js.executeScript("arguments[0].click();", homePage.emailFriend);
+
+		new Actions(driver);
+		act.moveToElement(homePage.recipientName).sendKeys("Mustaq");
+		new Actions(driver);
+		act.moveToElement(homePage.recipientEmail).sendKeys("test.automation+SeleniumTest@build.com");
+		new Actions(driver);
+		act.moveToElement(homePage.yourName).sendKeys("This is Alex Reid, sending you a cart from my automation!");
+		new Actions(driver);
+		act.moveToElement(homePage.yourEmail).sendKeys("atreidit23@gmail.com");
+		js.executeScript("arguments[0].click();", homePage.myCopy);
+		js.executeScript("arguments[0].click();", homePage.send);
+
+		File screenshotAs = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+		FileUtils.copyFile(screenshotAs, new File("screenshot.png"));
+
+
+		softly.assertThat(homePage.onBuildTheme())
+				.as("The \"Email sent!\" success message is displayed after emailing the cart")
+				.isTrue();
+
+
+
 	}
 
 	/**
